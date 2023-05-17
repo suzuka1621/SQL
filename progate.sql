2abf7b0d05b2469b47070fb85d7db158219ff9aa
suzuka1621 <akaneakua0625@gmail.com>
2023-05-17 21:33:18 +0900

    データの型による取得

--- progate.sql
+++ progate.sql
@@ -22,3 +22,15 @@ FROM purchases;
 SELECT *
 FROM purchases
 WHERE category = "食費";
+
+
+-- 「FROM purchases」のあとにコードを追加し、priceカラムが「1000」のデータを取得してください
+SELECT *
+FROM purchases
+WHERE price = 1000;
+
+
+-- 「FROM purchases」のあとにコードを追加し、purchased_atカラムが「2017-07-01」のデータを取得してください
+SELECT *
+FROM purchases
+WHERE purchased_at = "2017-07-01";
