6e146cff65df890fb02cc8b895ea54362db3d61a
suzuka1621 <akaneakua0625@gmail.com>
2023-05-17 22:08:47 +0900

    LIMIT

--- progate.sql
+++ progate.sql
@@ -115,3 +115,16 @@ SELECT *
 FROM purchases
 WHERE character_name = "にんじゃわんこ"
 ORDER BY price ASC;
+
+
+-- 「FROM purchases」のあとにコードを追加し、取得結果が「最大5件」となるようにデータを取得してください。
+SELECT *
+FROM purchases
+LIMIT 5;
+
+
+-- 「WHERE character_name = "にんじゃわんこ"」のあとにコードを追加し、取得結果が「最大10件」となるようにデータを取得してください。
+SELECT *
+FROM purchases
+WHERE character_name = "にんじゃわんこ"
+LIMIT 10;
