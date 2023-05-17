fe0deb24009521c7a1e8e8788744c1dc155c8c9f
suzuka1621 <akaneakua0625@gmail.com>
2023-05-17 21:25:31 +0900

    複数のカラムを取得する方法

--- progate.sql
+++ progate.sql
@@ -1,9 +1,18 @@
 -- purchasesテーブルから、nameカラムのデータを取得してください
-
 SELECT name
 FROM purchases;
 
 
 -- purchasesテーブルから、priceカラムのデータを取得してください
 SELECT price
+FROM purchases;
+
+
+-- purchasesテーブルから、「nameカラム」と「priceカラム」のデータを取得してください
+SELECT name, price
+FROM purchases;
+
+
+-- purchasesテーブルから、全てのカラムのデータを取得してください
+SELECT *
 FROM purchases;
