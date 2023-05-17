e26965b070f6a22378c5ee457ccb761f79a8163d
suzuka1621 <akaneakua0625@gmail.com>
2023-05-17 21:38:06 +0900

    比較演算子

--- progate.sql
+++ progate.sql
@@ -34,3 +34,15 @@ WHERE price = 1000;
 SELECT *
 FROM purchases
 WHERE purchased_at = "2017-07-01";
+
+
+-- 「FROM purchases」のあとにコードを追加し、priceカラムが「1000以上」のデータを取得してください
+SELECT *
+FROM purchases
+WHERE price >= 1000;
+
+
+-- 「FROM purchases」のあとにコードを追加し、purchased_atカラムが「2017-08-01以前」のデータを取得してください
+SELECT *
+FROM purchases
+WHERE purchased_at <= "2017-08-01";
