84c7cc60f396e2d074f87907c9239e9cd5654ef1
suzuka1621 <akaneakua0625@gmail.com>
2023-05-17 21:29:01 +0900

    特定のデータを取り出す方法

--- progate.sql
+++ progate.sql
@@ -16,3 +16,9 @@ FROM purchases;
 -- purchasesテーブルから、全てのカラムのデータを取得してください
 SELECT *
 FROM purchases;
+
+
+-- 「FROM purchases」のあとにコードを追加し、categoryカラムが「食費」であるデータを取得してください
+SELECT *
+FROM purchases
+WHERE category = "食費";
